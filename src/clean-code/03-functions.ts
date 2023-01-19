bbbb17ed1653da1c67fa70aaab8d85f1b86ffdfb
AlexisleoMena <alexismena2690@gmail.com>
2023-01-19 20:13:23 -0300

    Ejercicio 03-functions continuacion

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -59,5 +59,29 @@
     console.log('Crear actor', birthdate);
     return true;        
   }
+  //Mejorar Funcion
+  // const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+  //   let result;
+  //   if ( isDead ) {
+  //       result = 1500;
+  //   } else {
+  //       if ( isSeparated ) {
+  //           result = 2500;
+  //       } else {
+  //           if ( isRetired ) {
+  //               result = 3000;
+  //           } else {
+  //               result = 4000; 
+  //           }
+  //       }
+  //   }
+    
+  //   return result;
+  // }
+  const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+    if (isDead) return 1500;
+    if (isSeparated) return 2500;
+    return isRetired ? 3000 : 4000;
+  }
 
 })();
