a92bb0c649ae5bd85ed228643e2f6990333ebaea
AlexisleoMena <alexismena2690@gmail.com>
2023-01-19 20:07:24 -0300

    Ejercicio 03-functions

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,63 @@
+(() => {
+
+  // función para obtener información de una película por Id
+  // function getAllMovies( movieId: string ) {
+  //     console.log({ movieId });
+  // }
+  function getMovieById( id: string ) {
+    console.log({ id });
+}
+
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  // function getAllMovieActors( id: string ) {
+  //     console.log({ id });
+  // }
+  function getMovieCastById( id: string ) {
+    console.log({ id });
+  }
+
+  // funcion para obtener el bio del actor por el id
+  // function getUsuario( ActorId: string ) {
+  //     console.log({ ActorId });
+  // }
+  function getActorBioById( id: string ) {
+    console.log({ id });
+  }
+  
+  // Crear una película
+  // function movie(title: string, description: string, rating: number, cast: string[] ) {
+  //     console.log({ title, description, rating, cast });
+  // }
+  interface Movie {
+    title: string;
+    description: string;
+    rating: number;
+    cast: string[];
+  }
+  function createMovie({title, description, rating, cast} : Movie ) {
+    console.log({ title, description, rating, cast });
+  }
+
+  // Crea un nuevo actor
+  // function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+      
+  //     // tarea asincrona para verificar nombre
+  //     // ..
+  //     // ..
+  //     if ( fullName === 'fernando' ) return false;
+
+  //     console.log('Crear actor');
+  //     return true;        
+
+  // }
+  function checkFullname(fullName:string) {
+    console.log({ fullName })
+    return true;
+  }
+  function createActor( fullName: string, birthdate: Date ): boolean {
+    if ( checkFullname(fullName) ) return false;
+    console.log('Crear actor', birthdate);
+    return true;        
+  }
+
+})();
