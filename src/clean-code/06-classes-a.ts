176356493595456726d1b5a935fba6054409eb65
AlexisleoMena <alexismena2690@gmail.com>
2023-01-19 22:18:46 -0300

    06-classes-a herencia vs principio de resposabilidad unica

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -3,22 +3,67 @@
   type Gender ='M'|'F'
 
   class Person {
-    // public name: string;
-    // public gender: Gender;
-    // public birthdate: Date
-    
-    // constructor(name: string, gender: Gender, birthdate: Date){
-    //   this.name = name;
-    //   this.gender = gender;
-    //   this.birthdate = birthdate;
-    // }
     constructor(
-      public name: string,
-      public gender: Gender,
+      public name     : string, 
+      public gender   : Gender,
       public birthdate: Date
     ){}
   }
 
+  class User extends Person {
+    public lastAccess: Date;
+    constructor(
+      public email: string,
+      public role : string,
+      name        : string,
+      gender      : Gender,
+      birthDate   : Date
+    ){
+      super(name, gender, birthDate);
+      this.lastAccess = new Date()
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  //Clase que violenta el principio de responsabilidad única
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder  : string,
+      email                  : string,
+      role                   : string,
+      name                   : string,
+      gender                 : Gender,
+      birthDate              : Date
+    ){
+      super(email, role, name, gender, birthDate);
+    }
+  }
+
   const newPerson = new Person('Alexis', 'M', new Date('1995-10-02'))
   console.log(newPerson)
+
+  const newUser = new User( 
+   'alexismena2690@gmail.com',
+   'Admin', 
+   'Alexis', 
+   'M', 
+    new Date('1995-10-02')
+  )
+  console.log(newUser)
+
+  const userSettings = new UserSettings(
+    '/user/home',
+    '/home',
+    'alexismena2690@gmail.com',
+    'Admin',
+    'Alexis',
+    'M',
+    new Date('1995-10-02'),
+  )
+  console.log(userSettings)
+
 })()
